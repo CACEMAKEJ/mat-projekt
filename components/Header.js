2a6d11c593ff14208d7d02a724c1ffad3c2140d8
Martin Macicha <macima@delta-studenti.cz>
2020-10-15 21:20:31 +0200

    about section start

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,11 @@ const Header = () =>
         <img src='/logo.svg' alt='Rehamza' width='200' height='200' className='header-logo-svg'></img>
     </div>
     <div className='header-links'>
-        <span><a href='#'>O Nás</a></span>
-        <span><a href='#'>Projekty</a></span>
-        <span><a href='#'>Tým</a></span>
-        <span><a href='#'>Kontakt</a></span>
-        <button className='bt-login'></button>
+        <span><a href='#' class='header-link'>O Nás</a></span>
+        <span><a href='#' class='header-link'>Projekty</a></span>
+        <span><a href='#' class='header-link'>Tým</a></span>
+        <span><a href='#' class='header-link'>Kontakt</a></span>
+        <span><a href='#' class='bt-login'>Přihlaš se</a></span>
     </div>
 </div>
 export default Header
