a1e3f329cd0a4c80f36f9624f17317a618d32d07
Martin Macicha <macima@delta-studenti.cz>
2020-12-18 14:27:29 +0100

    YEP
    YEP

--- firebaseSetup.js
+++ firebaseSetup.js
@@ -2,17 +2,17 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyARvR5IoBnOtRJ8g5Djfr7PKeXtGk4s1So",
-    authDomain: "rehamza-mprj.firebaseapp.com",
-    databaseURL: "https://rehamza-mprj.firebaseio.com",
-    projectId: "rehamza-mprj",
-    storageBucket: "rehamza-mprj.appspot.com",
-    messagingSenderId: "112940981545",
-    appId: "1:112940981545:web:24289e2e63079c082fab38"
+  apiKey: 'AIzaSyDdW-Wl6GiIt5nRI8J7dncIXPEaFFb16x4',
+  authDomain: 'rehamza-mprj.firebaseapp.com',
+  databaseURL: 'https://rehamza-mprj.firebaseio.com',
+  projectId: 'rehamza-mprj',
+  storageBucket: 'rehamza-mprj.appspot.com',
+  messagingSenderId: '112940981545',
+  appId: '1:112940981545:web:24289e2e63079c082fab38',
 };
 
-if(firebase.apps.length === 0){
-    firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
 
 export default firebase;
