316c9c827e54d884aa09302f5d9ee9daa9c1cb98
Martin Macicha <macima@delta-studenti.cz>
2021-01-07 23:13:23 +0100

    fix(team): responsivity

--- components/Team.js
+++ components/Team.js
@@ -2,18 +2,30 @@ import { Image } from 'semantic-ui-react';
 
 const Team = () => (
   <div className='team' id='team'>
-    <div className='team-info'>
+    <div className='team-text'>
       <h2>Ing. Pavel Čáp</h2>
-      <p>Od roku 1996 se věnuje programování.</p>
-      <p>
-        Od roku 2013 začal vyvíjet rehabilitační pomůcky společně s Hamzovou
-        léčebnou.
-      </p>
-      <p>
-        V roce 2015 dostal cenu Duhové Křídlo za vývoj rehabilitačních pomůcek a
-        vedení žáků k pomoci druhým.
-      </p>
-      <p>Od roku 2017 získal ocenění Microsoft Innovative Educator Expert.</p>
+      <ul>
+        <li>
+          <p>Od roku 1996 se věnuje programování.</p>
+        </li>
+        <li>
+          <p>
+            Od roku 2013 začal vyvíjet rehabilitační pomůcky společně s Hamzovou
+            léčebnou.
+          </p>
+        </li>
+        <li>
+          <p>
+            V roce 2015 dostal cenu Duhové Křídlo za vývoj rehabilitačních
+            pomůcek a vedení žáků k pomoci druhým.
+          </p>
+        </li>
+        <li>
+          <p>
+            Od roku 2017 získal ocenění Microsoft Innovative Educator Expert.
+          </p>
+        </li>
+      </ul>
     </div>
     <div className='team-img'>
       <Image src='/cap.png'></Image>
